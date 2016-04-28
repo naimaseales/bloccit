4e9c6d1e5eb5934934e9d59f8293b3836cb216b8
Naima Seales <naimaoni@gmail.com>
2016-04-28 05:06:55 -0400

    checkpoint 39 not complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,14 @@
 require 'random_data'
 
+5.times do
+  User.create!(
+  name:     RandomData.random_name,
+  email:    RandomData.random_email,
+  password: RandomData.random_sentence
+  )
+end
+users = User.all
+
 15.times do
   Topic.create!(
     name:         RandomData.random_sentence,
@@ -11,9 +20,10 @@ topics = Topic.all
 #create posts
 50.times do
   Post.create!(
-    topic: topics.sample,
-    title: RandomData.random_sentence,
-    body: RandomData.random_paragraph
+    user:   users.sample,
+    topic:  topics.sample,
+    title:  RandomData.random_sentence,
+    body:   RandomData.random_paragraph
   )
 end
 posts = Post.all
@@ -26,7 +36,14 @@ posts = Post.all
   )
 end
 
+user = User.first
+user.update_attributes!(
+  email: 'naimaoni@gmail.com',
+  password: 'helloworld'
+)
+
 puts "Seed finished"
+puts "#{User.count} users created"
 puts "#{Topic.count} topics created"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
