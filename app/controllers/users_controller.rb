d0f833c6328b5d09a51a3edcc53919e6074f1954
Naima Seales <naimaoni@gmail.com>
2016-04-28 05:42:13 -0400

    assigment 37 signup: revised

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new
     @user.name = params[:user][:name]
+    @user.email = params[:user][:email]
+    @user.password = params[:user][:password]
+    @user.password_confirmation = params[:user][:password_confirmation]
 
     if @user.save
       flash[:notice] = "Welcome to Bloccit, #{@user.name}!"
