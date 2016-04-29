2d81d3a173a4c2f4956e817f487aa25a806d7004
Naima Seales <naimaoni@gmail.com>
2016-04-29 05:44:06 -0400

    checkpoint-40-auth: complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -36,9 +36,22 @@ posts = Post.all
   )
 end
 
-user = User.first
-user.update_attributes!(
-  email: 'naimaoni@gmail.com',
+# user = User.first
+# user.update_attributes!(
+#   email: 'naimaoni@gmail.com',
+#   password: 'helloworld'
+# )
+
+admin = User.create!(
+  name:     'Admin User',
+  email:    'admin@example.com',
+  password: 'helloworld',
+  role:     'admin'
+)
+
+member = User.create!(
+  name:     'Member User',
+  email:    'member@example.com',
   password: 'helloworld'
 )
 
