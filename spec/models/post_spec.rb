d5c3cb62a3654236712789a9747cc3ae3b2b59cb
Naima Seales <naimaoni@gmail.com>
2016-05-04 05:06:10 -0400

    checkpoint 44 started

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -25,6 +25,7 @@ RSpec.describe Post, type: :model do
   it { is_expected.to validate_length_of(:body).is_at_least(20) }
   it { is_expected.to have_many(:comments) }
   it { is_expected.to have_many(:votes) }
+  it { is_expected.to have_many(:favorites) }
 
   describe "attributes" do
     it "has a title, body and user attribute" do
