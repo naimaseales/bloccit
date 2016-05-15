0d7a8da7520c4eba0cda818d2c825aefd57184f5
Naima Seales <naimaoni@gmail.com>
2016-05-15 16:47:57 -0400

    assignmnet-48-api: complete

--- spec/api/v1/controllers/comments_controller_spec.rb
+++ spec/api/v1/controllers/comments_controller_spec.rb
@@ -1,25 +1,26 @@
 require 'rails_helper'
 
-RSpec.describe Api::V1::TopicsController, type: :controller do
+RSpec.describe Api::V1::CommentsController, type: :controller do
   let(:my_user) { create(:user) }
-  let(:my_topic) { create(:topic) }
-  let(:my_post) { create(:post, topic: my_topic, user: my_user) }
+  let(:my_post) { create(:post) }
+  let(:my_comment) { Comment.create!(body: RandomData.random_sentence, post: my_post, user: my_user) }
 
   context "unauthenticated user" do
-    it "GET index returns http success" do
+    it "GET index returns success" do
       get :index
       expect(response).to have_http_status(:success)
     end
 
     it "GET show returns http success" do
-      get :show, id: my_topic.id
+      get :show, id: my_comment.id
       expect(response).to have_http_status(:success)
     end
 
-    it "GET show returns child posts" do
-      get :show, id: my_topic.id
+    it "GET show returns comments" do
+      get :show, id: my_comment.id
       hashed_response = JSON.parse response.body
-      expect(hashed_response['posts']).to_not be_nil
+      expect(hashed_response['id']).to eq my_comment.id
+      expect(hashed_response['body']).to eq my_comment.body
     end
   end
 
@@ -28,20 +29,21 @@ RSpec.describe Api::V1::TopicsController, type: :controller do
       controller.request.env['HTTP_AUTHORIZATION'] = ActionController::HttpAuthentication::Token.encode_credentials(my_user.auth_token)
     end
 
-    it "GET index returns http" do
+    it "GET index returns http success" do
       get :index
       expect(response).to have_http_status(:success)
     end
 
     it "GET show returns http success" do
-      get :show, id: my_topic.id
+      get :show, id: my_comment.id
       expect(response).to have_http_status(:success)
     end
 
-    it "GET show returns child posts" do
-      get :show, id: my_topic.id
+    it "GET show returns comments" do
+      get :show, id: my_comment.id
       hashed_response = JSON.parse response.body
-      expect(hashed_response['posts']).to_not be_nil
+      expect(hashed_response['id']).to eq my_comment.id
+      expect(hashed_response['body']).to eq my_comment.body
     end
   end
 end
--- spec/api/v1/controllers/posts_controller_spec.rb
+++ spec/api/v1/controllers/posts_controller_spec.rb
@@ -1,25 +1,26 @@
 require 'rails_helper'
 
-RSpec.describe Api::V1::TopicsController, type: :controller do
+RSpec.describe Api::V1::PostsController, type: :controller do
   let(:my_user) { create(:user) }
   let(:my_topic) { create(:topic) }
   let(:my_post) { create(:post, topic: my_topic, user: my_user) }
+  let(:my_comment) { Comment.create!(body: RandomData.random_paragraph, post: my_post, user: my_user) }
 
   context "unauthenticated user" do
-    it "GET index returns http success" do
+    it "GET index returns success" do
       get :index
       expect(response).to have_http_status(:success)
     end
 
     it "GET show returns http success" do
-      get :show, id: my_topic.id
+      get :show, id: my_post.id
       expect(response).to have_http_status(:success)
     end
 
-    it "GET show returns child posts" do
-      get :show, id: my_topic.id
+    it "GET show returns child comments" do
+      get :show, id: my_post.id
       hashed_response = JSON.parse response.body
-      expect(hashed_response['posts']).to_not be_nil
+      expect(hashed_response['comments']).to_not be_nil
     end
   end
 
@@ -28,20 +29,20 @@ RSpec.describe Api::V1::TopicsController, type: :controller do
       controller.request.env['HTTP_AUTHORIZATION'] = ActionController::HttpAuthentication::Token.encode_credentials(my_user.auth_token)
     end
 
-    it "GET index returns http" do
+    it "GET index returns http success" do
       get :index
       expect(response).to have_http_status(:success)
     end
 
     it "GET show returns http success" do
-      get :show, id: my_topic.id
+      get :show, id: my_post.id
       expect(response).to have_http_status(:success)
     end
 
-    it "GET show returns child posts" do
-      get :show, id: my_topic.id
+    it "GET show returns child comments" do
+      get :show, id: my_post.id
       hashed_response = JSON.parse response.body
-      expect(hashed_response['posts']).to_not be_nil
+      expect(hashed_response['comments']).to_not be_nil
     end
   end
 end
