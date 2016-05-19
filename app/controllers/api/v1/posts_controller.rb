4531bf1cb3ec9c88845c937b1302219d69ecb0b3
Naima Seales <naimaoni@gmail.com>
2016-05-18 20:08:02 -0400

    assignment-49-api complete

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -16,7 +16,9 @@ class Api::V1::PostsController < Api::V1::BaseController
   def update
     post = Post.find(params[:id])
 
-    if post.update_attributes(post_params)
+    if post.user != @current_user && !@current_user.admin? 
+      render json: { error: "Not Authorized", status: 403 }, status: 403
+    elsif post.update_attributes(post_params)
       render json: post.to_json, status: 200
     else
       render json: {error: "Post update failed", status: 400}, status: 400
@@ -24,13 +26,15 @@ class Api::V1::PostsController < Api::V1::BaseController
   end
 
   def create
-    topic = Topic.find(params[topic.id])
-    post = topic.posts(post_params)
+    topic = Topic.find(params[:topic_id])
+    post = topic.posts.new(post_params)
+    post.user = @current_user
 
     if post.valid?
       post.save!
       render json: post.to_json, status: 201
     else
+      p post.errors
       render json: {error: "Post in invalid", status: 400}, status: 400
     end
   end
@@ -38,7 +42,9 @@ class Api::V1::PostsController < Api::V1::BaseController
   def destroy
     post = Post.find(params[:id])
 
-    if post.destroy
+    if post.user != @current_user
+      render json: { error: "Not Authorized", status: 403 }, status: 403
+    elsif post.destroy
       render json: {message: "Post destroyed", status: 200}, status: 200
     else
       render json: {error: "Post destroy failed", status: 400}, status: 400
@@ -46,6 +52,13 @@ class Api::V1::PostsController < Api::V1::BaseController
   end
 
   private
+
+  def authorize_user
+    unless @current_user
+      render json: { error: "Not Authorized", status: 403 }, status: 403
+    end
+  end
+
   def post_params
     params.require(:post).permit(:title, :body)
   end
--- spec/api/v1/controllers/posts_controller_spec.rb
+++ spec/api/v1/controllers/posts_controller_spec.rb
@@ -6,12 +6,8 @@ RSpec.describe Api::V1::PostsController, type: :controller do
   let(:my_post) { create(:post, topic: my_topic, user: my_user) }
 
   context "unauthenticated user" do
-    before do
-      controller.request.env['HTTP_AUTHORIZATION'] = ActionController::HttpAuthentication::Token.encode_credentials(my_user.auth_token)
-    end
 
     it "PUT update returns http unauthenticated" do
-      new_post = build(:post)
       put :update, id: my_post.id, post: {title: "Post Title", body: "Post Body"}
       expect(response).to have_http_status(401)
     end
@@ -30,20 +26,15 @@ RSpec.describe Api::V1::PostsController, type: :controller do
 
   context "unauthorized user" do
     before do
-      controller.request.env['HTTP_AUTHORIZATION'] = ActionController::HttpAuthentication::Token.encode_credentials(my_user.auth_token)
+      non_author = create(:user)
+      controller.request.env['HTTP_AUTHORIZATION'] = ActionController::HttpAuthentication::Token.encode_credentials(non_author.auth_token)
     end
 
-    it "PUT update returns http forbidden" do
-      new_post = build(:post)
+    it "PUT update returns http forbidden for non-author" do
       put :update, id: my_post.id, post: {title: "Post Title", body: "Post Body"}
       expect(response).to have_http_status(403)
     end
 
-    it "POST create returns http forbidden" do
-      new_post = build(:post)
-      post :create, topic_id: my_topic.id, post: {title: "Post Title", body: "Post Body"}
-      expect(response).to have_http_status(403)
-    end
 
     it "DELETE destroy returns http forbidden" do
       delete :destroy, id: my_post.id
@@ -51,9 +42,13 @@ RSpec.describe Api::V1::PostsController, type: :controller do
     end
   end
 
-  context "unauthenticated and authorized users" do
-    describe "POST create" do
+  context "authenticated and authorized users" do
+    before do
+      controller.request.env['HTTP_AUTHORIZATION'] = ActionController::HttpAuthentication::Token.encode_credentials(my_user.auth_token)
+    end
 
+    describe "POST create" do
+      before {post :create, topic_id: my_topic.id, post: {title: "Post Title", body: RandomData.random_paragraph} }
       it "returns http success" do
         expect(response).to have_http_status(:success)
       end
@@ -72,6 +67,24 @@ RSpec.describe Api::V1::PostsController, type: :controller do
 
       it "returns json content type" do
         expect(response.content_type).to eq 'application/json'
+      end
+    end
+  end
+
+  context "admin users" do
+    before do
+      admin_user = create(:user, role: :admin)
+      controller.request.env['HTTP_AUTHORIZATION'] = ActionController::HttpAuthentication::Token.encode_credentials(admin_user.auth_token)
+    end
+
+    describe "PUT update" do
+      it "updates the post" do
+        put :update, id: my_post.id, post: {title: "New Post Title", body: 'This is a new body for the post'}
+        expect(response).to have_http_status(:success)
+        expect(response.content_type).to eq 'application/json'
+        json = JSON.parse(response.body)
+        expect(json["title"]).to eq "New Post Title"
+        expect(json["body"]).to eq "This is a new body for the post"
       end
     end
   end
